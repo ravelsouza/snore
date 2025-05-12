756c5d978dffc23941fcf44c808e50312f3484f6
Ravel <ravelsouza.eletromecanica@gmail.com>
2025-05-12 13:06:26 -0300

    third commit

--- app.py
+++ app.py
@@ -1,6 +1,20 @@
 from flask import Flask, request, jsonify
+import numpy as np
+import librosa
+import tensorflow as tf
+from sklearn.preprocessing import StandardScaler
+import joblib
+from flask_cors import CORS
+import io
+import base64
+import soundfile as sf
 
 app = Flask(__name__)
+CORS(app)
+
+interpreter = tf.lite.Interpreter(model_path="model.tflite")
+interpreter.allocate_tensors()
+scaler = joblib.load("scaler.pkl")
 
 @app.route('/', methods=['GET'])
 def teste():
